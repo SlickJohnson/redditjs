726360bb5cc477e511ef54a096db56b8d6caafc8
SlickJohnson <liwa.johnson@gmail.com>
2018-11-06 15:56:05 -0800

    Update posts/new to use Post model to save to db

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,12 @@
+const Post = require('../models/post');
+
 module.exports = app => {
   // CREATE
   app.post('/posts/new', (req, res) => {
-    console.log(req.body);
+    const post = new Post(req.body);
+
+    post.save((err, post) => {
+      return res.redirect(`/`);
+    });
   });
 };
